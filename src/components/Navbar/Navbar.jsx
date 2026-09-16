9eb421bc3e22c7f4c4ebe1bd377d39f86508d3c8
Eryck <fernandoeryck0@gmail.com>
2026-09-16 13:16:46 -0300

    Feito

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,44 +1,102 @@
+
 import "./Navbar.css"
 
-function Navbar({ user = "Usuário", onSearch }) {
+import useAuth from "../../hooks/useAuth"
+
+function Navbar({ onSearch }) {
+
+    const { user } = useAuth()
+
+    const username =
+        user?.user_metadata?.username ||
+        user?.user_metadata?.name ||
+        user?.email?.split("@")[0] ||
+        "Usuário"
+
+    const avatarLetter =
+        username
+            .replace("@", "")
+            .charAt(0)
+            .toUpperCase()
+
+
     return (
+
         <header className="navbar">
 
             <div className="navbar-logo">
-                <span>START</span>
-                <strong>_UP</strong>
+
+                <span>
+                    START
+                </span>
+
+                <strong>
+                    _UP
+                </strong>
+
             </div>
 
+
             <div className="navbar-search">
+
                 <input
                     type="text"
                     placeholder="Pesquisar startups, pessoas ou ideias..."
-                    onChange={(event) => onSearch?.(event.target.value)}
+                    onChange={(event) =>
+                        onSearch?.(
+                            event.target.value
+                        )
+                    }
                 />
+
             </div>
+
 
             <div className="navbar-actions">
 
-                <button className="navbar-icon" title="Notificações">
+
+                <button
+                    className="navbar-icon"
+                    title="Notificações"
+                >
                     🔔
                 </button>
 
-                <button className="navbar-icon" title="Mensagens">
+
+                <button
+                    className="navbar-icon"
+                    title="Mensagens"
+                >
                     💬
                 </button>
 
+
                 <div className="navbar-user">
+
+
                     <div className="user-avatar">
-                        {user.charAt(0).toUpperCase()}
+
+                        {avatarLetter}
+
                     </div>
 
-                    <span>{user}</span>
+
+                    <span>
+
+                        {username}
+
+                    </span>
+
+
                 </div>
+
 
             </div>
 
         </header>
+
     )
 }
 
 export default Navbar
+
